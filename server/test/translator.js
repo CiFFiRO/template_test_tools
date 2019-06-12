e580016d4f68cec4e0d956cdc6a9e9705c9bff17
qwe <new_mail_conf@protonmail.com>
2019-06-12 20:30:55 +0300

    server: added tests for translator module functions.

---
+++ server/test/translator.js
@@ -0,0 +1,59 @@
+const ASSERT = require('chai').assert;
+const FS = require('fs');
+const translator = require('../translator.js');
+
+function test_positiveCheckTTT() {
+  const prefixTTTFileName = 'server/test/ttt/ttt_';
+  const testNumber = 1;
+  for (let i = 0; i < testNumber; ++i) {
+    let fileName = prefixTTTFileName;
+    if (i<10) {
+      fileName += '0' + i;
+    } else {
+      fileName += i;
+    }
+    fileName += '.json';
+
+    FS.readFile(fileName, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      let ttt = JSON.parse(data);
+      ASSERT.doesNotThrow(function (){translator.checkTTT(ttt);});
+    });
+  }
+}
+
+function test_positiveGenerateTestTaskFromTTT() {
+  const prefixTTTFileName = 'server/test/ttt/ttt_';
+  const testNumber = 1;
+  for (let i = 0; i < testNumber; ++i) {
+    let fileName = prefixTTTFileName;
+    if (i<10) {
+      fileName += '0' + i;
+    } else {
+      fileName += i;
+    }
+    fileName += '.json';
+
+    FS.readFile(fileName, (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      let ttt = JSON.parse(data);
+      ASSERT.doesNotThrow(function (){translator.generateTestTaskFromTTT(ttt);});
+    });
+  }
+}
+
+function run() {
+  test_positiveCheckTTT();
+  test_positiveGenerateTestTaskFromTTT();
+}
+
+
+run();
