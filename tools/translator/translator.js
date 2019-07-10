28168e12c88d314f3183b4b25b7fc51d95a0efc2
qwe <new_mail_conf@protonmail.com>
2019-07-10 03:15:13 +0300

    translator: add checkTemplateTest and generateTestFormTemplateTest.

--- tools/translator/translator.js
+++ tools/translator/translator.js
@@ -51,6 +51,9 @@ const INPUT_TYPE = 0;
 const SINGLE_CHOOSE_TYPE = 1;
 const MULTIPLE_CHOOSE_TYPE = 2;
 
+const STRONG_ORDER_TYPE = 0;
+const RANDOM_ORDER_TYPE = 1;
+
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -69,6 +72,19 @@ function rElement(elements) {
   return elements[getRandomInt(0, elements.length)];
 }
 
+function rSubArray(array, length) {
+  let result = [];
+  let copy = array.slice(0);
+  for (let _ = 0;_ < length;++_) {
+    if (copy.length === 0) {
+      break;
+    }
+    let i = getRandomInt(0, copy.length);
+    result.push(copy[i]);
+    copy.splice(i, 1);
+  }
+  return result;
+}
 
 function isDigit(aChar) {
   let myCharCode = aChar.charCodeAt(0);
@@ -100,258 +116,299 @@ function isSpace(aChar) {
   return false;
 }
 
-module.exports = {
-  checkTTT: function (ttt) {
-    if (!ttt.hasOwnProperty('title') || !ttt.hasOwnProperty('type') || !ttt.hasOwnProperty('rules')
-      || !ttt.hasOwnProperty('testText') || !ttt.hasOwnProperty('answerScript')) {
-      throw Error('Bad json data');
-    }
-    if (typeof ttt['type'] !== 'number' || ttt['type'] < 0 || ttt['type'] > 2) {
-      throw Error('Bad TTT type');
-    }
-    let rules = new Set();
-    for (let rule in ttt['rules']) {
-      if (ttt['rules'].hasOwnProperty(rule)) {
-        if (!/^[0-9a-zA-Z]+$/.test(rule)) {
-          throw Error('Rule name has syntax error - ' + rule);
-        }
-        if (rules.has(rule)) {
-          throw Error('Duplicate rules name - ' + rule);
-        }
-        rules.add(rule);
-        let regexp = new RegExp('^([^$]|\\$\\$|\\$(' + SPECIAL_FUNCTIONS_RE + ')+)*$');
-        for (let i = 0; i < ttt['rules'][rule].length; ++i) {
-          if (!regexp.test(ttt['rules'][rule][i])) {
-            throw Error('Production has syntax error in rule - ' + rule);
-          }
+function  checkTTT (ttt) {
+  if (!ttt.hasOwnProperty('title') || !ttt.hasOwnProperty('type') || !ttt.hasOwnProperty('rules')
+    || !ttt.hasOwnProperty('testText') || !ttt.hasOwnProperty('answerScript')) {
+    throw Error('Bad json data');
+  }
+  if (typeof ttt['type'] !== 'number' || ttt['type'] < 0 || ttt['type'] > 2) {
+    throw Error('Bad TTT type');
+  }
+  let rules = new Set();
+  for (let rule in ttt['rules']) {
+    if (ttt['rules'].hasOwnProperty(rule)) {
+      if (!/^[0-9a-zA-Z]+$/.test(rule)) {
+        throw Error('Rule name has syntax error - ' + rule);
+      }
+      if (rules.has(rule)) {
+        throw Error('Duplicate rules name - ' + rule);
+      }
+      rules.add(rule);
+      let regexp = new RegExp('^([^$]|\\$\\$|\\$(' + SPECIAL_FUNCTIONS_RE + ')+)*$');
+      for (let i = 0; i < ttt['rules'][rule].length; ++i) {
+        if (!regexp.test(ttt['rules'][rule][i])) {
+          throw Error('Production has syntax error in rule - ' + rule);
         }
       }
     }
+  }
 
-    let ruleNames = '';
-    for (let name of rules) {
-      ruleNames += name + '|';
-    }
-    ruleNames = ruleNames.substr(0, ruleNames.length - 1);
+  let ruleNames = '';
+  for (let name of rules) {
+    ruleNames += name + '|';
+  }
+  ruleNames = ruleNames.substr(0, ruleNames.length - 1);
 
-    let checkRegExpPattern = '^([^$]|\\$\\$|\\$\\{\\s*(' + ruleNames + ')\\s*\\}+)*$';
-    let checkRegExp = new RegExp(checkRegExpPattern);
-    if (!checkRegExp.test(ttt['testText'])) {
-      throw new Error('Test text has syntax error')
-    }
-  },
-  generateTestTaskFromTTT: function (ttt) {
-    let result = {};
-    result['type'] = ttt['type'];
+  let checkRegExpPattern = '^([^$]|\\$\\$|\\$\\{\\s*(' + ruleNames + ')\\s*\\}+)*$';
+  let checkRegExp = new RegExp(checkRegExpPattern);
+  if (!checkRegExp.test(ttt['testText'])) {
+    throw new Error('Test text has syntax error')
+  }
+}
 
-    function replaceSpecialFunctions(production) {
-      function nextSpecialFunction(production) {
-        let i = 0;
-        while (i < production.length) {
-          if (i + 1 < production.length && production[i] === '$' && production[i + 1] !== '$') {
-            let startIndex = i;
-            let name = '';
+function generateTestTaskFromTTT (ttt) {
+  let result = {};
+  result['type'] = ttt['type'];
+
+  function replaceSpecialFunctions(production) {
+    function nextSpecialFunction(production) {
+      let i = 0;
+      while (i < production.length) {
+        if (i + 1 < production.length && production[i] === '$' && production[i + 1] !== '$') {
+          let startIndex = i;
+          let name = '';
+          ++i;
+          while (isAlpha(production[i]) || isDigit(production[i])) {
+            name += production[i];
             ++i;
-            while (isAlpha(production[i]) || isDigit(production[i])) {
-              name += production[i];
-              ++i;
-            }
+          }
 
-            let arguments = [];
-            while (i < production.length && production[i] !== ')') {
-              if (production[i] !== '(' && production[i] !== ')' && production[i] !== ',' && !isSpace(production[i])) {
-                let argument = '';
-                while (production[i] !== ')' && production[i] !== ',' && !isSpace(production[i])) {
-                  argument += production[i];
-                  ++i;
-                }
-                arguments.push(argument);
-              } else {
+          let arguments = [];
+          while (i < production.length && production[i] !== ')') {
+            if (production[i] !== '(' && production[i] !== ')' && production[i] !== ',' && !isSpace(production[i])) {
+              let argument = '';
+              while (production[i] !== ')' && production[i] !== ',' && !isSpace(production[i])) {
+                argument += production[i];
                 ++i;
               }
+              arguments.push(argument);
+            } else {
+              ++i;
             }
-
-            return [startIndex, i, name, arguments];
-          } else {
-            ++i;
-          }
-        }
-
-        return null;
-      }
-
-      let result = production;
-      while (true) {
-        let info = nextSpecialFunction(result);
-        if (info === null) {
-          break;
-        }
-
-        let name = info[2];
-        let arguments = info[3];
-
-        if (name === 'rInteger') {
-          if (arguments.length !== 2) {
-            throw Error('rInteger: number of arguments is not 2');
-          }
-          if (isNaN(+arguments[0]) || isNaN(+arguments[1])) {
-            throw Error('rInteger: argument(s) not a number');
-          }
-          let min = +arguments[0];
-          let max = +arguments[1];
-          if (min > max) {
-            throw Error('rInteger: interval is not exist');
           }
 
-          let value = rInteger(min, max);
-          result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
-        } else if (name === 'rFloat') {
-          if (arguments.length !== 3) {
-            throw Error('rFloat: number of arguments is not 3');
-          }
-          if (isNaN(+arguments[0]) || isNaN(+arguments[1]) || isNaN(+arguments[2])) {
-            throw Error('rFloat: argument(s) not a number');
-          }
-          let min = +arguments[0];
-          let max = +arguments[1];
-          let length = +arguments[2];
-          if (min > max) {
-            throw Error('rFloat: interval is not exist');
-          }
-          if (length <= 0) {
-            throw Error('rFloat: length is not natural');
-          }
-
-          let value = rFloat(min, max, length);
-          result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
-        } else if (name === 'rElement') {
-          if (arguments.length === 0) {
-            throw Error('rElement: does not have arguments');
-          }
-
-          let value = arguments[getRandomInt(0, arguments.length)];
-          result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
+          return [startIndex, i, name, arguments];
         } else {
-          throw Error('Special function ' + name + ' is not exist');
+          ++i;
         }
       }
 
-      return result;
+      return null;
     }
 
-    let selectedProductionForRules = {};
-    let valueForRules = {};
+    let result = production;
+    while (true) {
+      let info = nextSpecialFunction(result);
+      if (info === null) {
+        break;
+      }
+
+      let name = info[2];
+      let arguments = info[3];
+
+      if (name === 'rInteger') {
+        if (arguments.length !== 2) {
+          throw Error('rInteger: number of arguments is not 2');
+        }
+        if (isNaN(+arguments[0]) || isNaN(+arguments[1])) {
+          throw Error('rInteger: argument(s) not a number');
+        }
+        let min = +arguments[0];
+        let max = +arguments[1];
+        if (min > max) {
+          throw Error('rInteger: interval is not exist');
+        }
+
+        let value = rInteger(min, max);
+        result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
+      } else if (name === 'rFloat') {
+        if (arguments.length !== 3) {
+          throw Error('rFloat: number of arguments is not 3');
+        }
+        if (isNaN(+arguments[0]) || isNaN(+arguments[1]) || isNaN(+arguments[2])) {
+          throw Error('rFloat: argument(s) not a number');
+        }
+        let min = +arguments[0];
+        let max = +arguments[1];
+        let length = +arguments[2];
+        if (min > max) {
+          throw Error('rFloat: interval is not exist');
+        }
+        if (length <= 0) {
+          throw Error('rFloat: length is not natural');
+        }
+
+        let value = rFloat(min, max, length);
+        result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
+      } else if (name === 'rElement') {
+        if (arguments.length === 0) {
+          throw Error('rElement: does not have arguments');
+        }
+
+        let value = arguments[getRandomInt(0, arguments.length)];
+        result = result.substring(0, info[0]) + value + result.substring(info[1]+1);
+      } else {
+        throw Error('Special function ' + name + ' is not exist');
+      }
+    }
+
+    return result;
+  }
+
+  let selectedProductionForRules = {};
+  let valueForRules = {};
+  for (let rule in ttt['rules']) {
+    if (ttt['rules'].hasOwnProperty(rule)) {
+      selectedProductionForRules[rule] = getRandomInt(0, ttt['rules'][rule].length);
+      valueForRules[rule] = replaceSpecialFunctions(ttt['rules'][rule][selectedProductionForRules[rule]]);
+    }
+  }
+
+  function replaceRules(text) {
+    let result = text;
     for (let rule in ttt['rules']) {
       if (ttt['rules'].hasOwnProperty(rule)) {
-        selectedProductionForRules[rule] = getRandomInt(0, ttt['rules'][rule].length);
-        valueForRules[rule] = replaceSpecialFunctions(ttt['rules'][rule][selectedProductionForRules[rule]]);
+        let regexpScheme = '\\$\\{\\s*' + rule + '\\s*\\}';
+        let regexp = new RegExp(regexpScheme, 'g');
+        result = result.replace(regexp, valueForRules[rule]);
       }
     }
 
-    function replaceRules(text) {
-      let result = text;
-      for (let rule in ttt['rules']) {
-        if (ttt['rules'].hasOwnProperty(rule)) {
-          let regexpScheme = '\\$\\{\\s*' + rule + '\\s*\\}';
-          let regexp = new RegExp(regexpScheme, 'g');
-          result = result.replace(regexp, valueForRules[rule]);
-        }
-      }
+    result = result.replace(/\$\$/g, '$');
 
-      result = result.replace(/\$\$/g, '$');
+    return result;
+  }
 
-      return result;
-    }
+  function removeEmptyStrings(text) {
+    let lines = text.split('\n');
+    let result = '';
 
-    function removeEmptyStrings(text) {
-      let lines = text.split('\n');
-      let result = '';
-
-      for (let lineIndex = 0; lineIndex < lines.length; ++lineIndex) {
-        if (lines[lineIndex].length > 0) {
-          result += lines[lineIndex] + '\n';
-        }
-      }
-
-      return result;
-    }
-
-    function replaceSpecialSymbolsGIFT(text) {
-      let result = text;
-      let specialCharacters = ['~', '=', '#', '{', '}', ':'];
-      for (let i=0;i<specialCharacters.length;++i) {
-        let regexp = new RegExp(specialCharacters[i], 'g');
-        result = result.replace(regexp, '\\'+specialCharacters[i]);
-      }
-
-      return result;
-    }
-
-    let testText = replaceSpecialSymbolsGIFT(removeEmptyStrings(replaceRules(ttt['testText'])));
-    if (testText.length === 0) {
-      throw Error('Test task text is empty');
-    }
-    
-    result['testText'] = testText;
-
-    let generatedAnswerScriptPart = 'let __TYPE_TEST_TASK = ' + ttt['type'] + ';\n';
-    for (let rule in ttt['rules']) {
-      if (ttt['rules'].hasOwnProperty(rule)) {
-        generatedAnswerScriptPart += 'let $' + rule + ' = ' + selectedProductionForRules[rule] + ';\n';
-        generatedAnswerScriptPart += 'let $' + rule + '_value = "' + valueForRules[rule].replace(/\n/g, '\\n') + '";\n';
-        for (let i=0;i<ttt['rules'][rule].length;++i) {
-          generatedAnswerScriptPart += 'let $' + rule + '_' + i + ' = ' + i + ';\n';
-        }
+    for (let lineIndex = 0; lineIndex < lines.length; ++lineIndex) {
+      if (lines[lineIndex].length > 0) {
+        result += lines[lineIndex] + '\n';
       }
     }
 
-    let script = generatedAnswerScriptPart + PREGENERATED_ANSWER_SCRIPT_PART + ttt['answerScript'] + ';\n__END_SCRIPT();';
-    let value = null;
-    try {
-      value = eval(script);
-    } catch (error) {
-      throw Error('Answer script error: ' + error.message);
+    return result;
+  }
+
+  function replaceSpecialSymbolsGIFT(text) {
+    let result = text;
+    let specialCharacters = ['~', '=', '#', '{', '}', ':'];
+    for (let i=0;i<specialCharacters.length;++i) {
+      let regexp = new RegExp(specialCharacters[i], 'g');
+      result = result.replace(regexp, '\\'+specialCharacters[i]);
     }
 
-    let answer = value[0];
-    let falseOptions = value[1];
+    return result;
+  }
 
-    if (typeof answer !== 'object') {
-      throw Error('Answer does not initialized');
-    }
-    if (answer.length < 1) {
-      throw Error('Wrong true answers number');
-    }
+  let testText = replaceSpecialSymbolsGIFT(removeEmptyStrings(replaceRules(ttt['testText'])));
+  if (testText.length === 0) {
+    throw Error('Test task text is empty');
+  }
 
-    answer.forEach(function (elem) {
-      let type = typeof elem;
-      if (type !== 'string' && type !== 'number') {
-        throw Error('Wrong true option type');
+  result['testText'] = testText;
+
+  let generatedAnswerScriptPart = 'let __TYPE_TEST_TASK = ' + ttt['type'] + ';\n';
+  for (let rule in ttt['rules']) {
+    if (ttt['rules'].hasOwnProperty(rule)) {
+      generatedAnswerScriptPart += 'let $' + rule + ' = ' + selectedProductionForRules[rule] + ';\n';
+      generatedAnswerScriptPart += 'let $' + rule + '_value = "' + valueForRules[rule].replace(/\n/g, '\\n') + '";\n';
+      for (let i=0;i<ttt['rules'][rule].length;++i) {
+        generatedAnswerScriptPart += 'let $' + rule + '_' + i + ' = ' + i + ';\n';
       }
-    });
-    falseOptions.forEach(function (elem) {
-      let type = typeof elem;
-      if (type !== 'string' && type !== 'number') {
-        throw Error('Wrong false option type');
-      }
-    });
+    }
+  }
 
-    if (ttt['type'] === SINGLE_CHOOSE_TYPE && answer.length > 1) {
-      throw Error('Multiple answer options');
+  let script = generatedAnswerScriptPart + PREGENERATED_ANSWER_SCRIPT_PART + ttt['answerScript'] + ';\n__END_SCRIPT();';
+  let value = null;
+  try {
+    value = eval(script);
+  } catch (error) {
+    throw Error('Answer script error: ' + error.message);
+  }
+
+  let answer = value[0];
+  let falseOptions = value[1];
+
+  if (typeof answer !== 'object') {
+    throw Error('Answer does not initialized');
+  }
+  if (answer.length < 1) {
+    throw Error('Wrong true answers number');
+  }
+
+  answer.forEach(function (elem) {
+    let type = typeof elem;
+    if (type !== 'string' && type !== 'number') {
+      throw Error('Wrong true option type');
+    }
+  });
+  falseOptions.forEach(function (elem) {
+    let type = typeof elem;
+    if (type !== 'string' && type !== 'number') {
+      throw Error('Wrong false option type');
+    }
+  });
+
+  if (ttt['type'] === SINGLE_CHOOSE_TYPE && answer.length > 1) {
+    throw Error('Multiple answer options');
+  }
+
+  if (ttt['type'] === INPUT_TYPE) {
+    result['answers'] = answer;
+  } else {
+    result['trueOptions'] = answer;
+
+    for (let i=0;i<answer.length;++i) {
+      if (falseOptions.indexOf(answer[i]) !== -1) {
+        throw Error('False options contain true option');
+      }
     }
 
-    if (ttt['type'] === INPUT_TYPE) {
-      result['answers'] = answer;
-    } else {
-      result['trueOptions'] = answer;
+    result['falseOptions'] = falseOptions;
+  }
 
-      for (let i=0;i<answer.length;++i) {
-        if (falseOptions.indexOf(answer[i]) !== -1) {
-          throw Error('False options contain true option');
-        }
+  return result;
+}
+
+module.exports = {
+  checkTTT: checkTTT,
+  generateTestTaskFromTTT: generateTestTaskFromTTT,
+  checkTemplateTest: function (templateTest) {
+    if (!templateTest.hasOwnProperty('orderType') || !templateTest.hasOwnProperty('arrayTTT') ||
+      !templateTest.hasOwnProperty('title')) {
+      return false;
+    }
+    if (typeof templateTest.orderType !== 'number' || typeof templateTest.arrayTTT !== 'object'
+    || typeof templateTest.title !== 'string') {
+      return false;
+    }
+    if (templateTest.orderType > RANDOM_ORDER_TYPE || templateTest.orderType < STRONG_ORDER_TYPE) {
+      return false;
+    }
+
+    for (let i=0;i<templateTest.arrayTTT.length;++i) {
+      try {
+        checkTTT(templateTest.arrayTTT[i]);
+      } catch (error) {
+        return false;
       }
+    }
 
-      result['falseOptions'] = falseOptions;
+    return true;
+  },
+  generateTestFormTemplateTest: function (templateTest) {
+    let result = [];
+    let arrayTTT = templateTest.arrayTTT.slice(0);
+
+    if (templateTest.orderType === RANDOM_ORDER_TYPE) {
+      arrayTTT = rSubArray(arrayTTT, arrayTTT.length);
+    }
+
+    for (let i=0;i<arrayTTT.length;++i) {
+      result.push(generateTestTaskFromTTT(arrayTTT[i]));
     }
 
     return result;
@@ -363,20 +420,6 @@ module.exports = {
     let result = preamble + '\n\n\n';
 
     function getOptions(trueOptions, falseOptions) {
-      function rSubArray(array, length) {
-        let result = [];
-        let copy = array.slice(0);
-        for (let _ = 0;_ < length;++_) {
-          if (copy.length === 0) {
-            break;
-          }
-          let i = getRandomInt(0, copy.length);
-          result.push(copy[i]);
-          copy.splice(i, 1);
-        }
-        return result;
-      }
-
       let result = [];
       for (let i=0;i<trueOptions.length;++i) {
         result.push([true, trueOptions[i]]);
