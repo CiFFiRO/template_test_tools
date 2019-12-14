e361c9821f7cf170fea0da4a0766230d2f3e300e
qwe <new_mail_conf@protonmail.com>
2019-12-14 14:23:33 +0300

    web_application: refactoring add grammar mark to task.

--- web_application/scripts/view/editors/template_task.js
+++ web_application/scripts/view/editors/template_task.js
@@ -15,7 +15,95 @@ class EditorTemplateTask {
       }, template => {
         return {header: template.title, grammar: template.grammar, testText: template.textTask};
       },text => {
-        return [];
+        if (text.length < 3) {
+          return [];
+        }
+
+        let result = [];
+        let colors = {ignoreSpecial: '#499c54',
+          link: '#cb602d',
+          noTerminal: '#ffc66d',
+          choose: '#4a88c7',
+          end: '#c63bb4'
+        };
+        let index = 0;
+        let line = 0, character = 0;
+        while (index < text.length) {
+          let begin = {line: line, ch: character};
+          while (index < text.length && text[index] !== '=') {
+            if (text[index] === '\n') {
+              ++line;
+              character = 0;
+              ++index;
+              continue;
+            }
+            ++character;
+            ++index;
+          }
+          result.push([begin, {line: line, ch: character}, colors.noTerminal]);
+          while (index < text.length) {
+            if (text[index] === '\n') {
+              ++line;
+              character = 0;
+              ++index;
+              continue;
+            }
+
+            begin = {line: line, ch: character};
+            if (text[index] === '|') {
+              result.push([begin, {line: line, ch: character+1}, colors.choose]);
+              ++character;
+              ++index;
+            }
+            else if (text[index] === '$') {
+              ++character;
+              ++index;
+
+              if (text[index] === '$' || text[index] === '|' || text[index] === '(' || text[index] === ')' ||
+                text[index] === '[' || text[index] === ']' || text[index] === '.' || text[index] === '=') {
+                result.push([begin, {line: line, ch: character+1}, colors.ignoreSpecial]);
+                ++character;
+                ++index;
+              } else if (text[index] === '{') {
+                result.push([begin, {line: line, ch: character+1}, colors.link]);
+                while (index < text.length && text[index] !== '}') {
+                  if (text[index] === '\n') {
+                    ++line;
+                    character = 0;
+                    ++index;
+                    continue;
+                  }
+                  ++character;
+                  ++index;
+                }
+                result.push([{line: begin.line, ch: begin.ch+2}, {line: line, ch: character}, colors.noTerminal]);
+                result.push([{line: line, ch: character}, {line: line, ch: character+1}, colors.link]);
+              } else {
+                while (index < text.length && text[index] !== ')') {
+                  if (text[index] === '\n') {
+                    ++line;
+                    character = 0;
+                    ++index;
+                    continue;
+                  }
+                  ++character;
+                  ++index;
+                }
+                result.push([begin, {line: line, ch: character+1}, colors.noTerminal]);
+              }
+            } else if (text[index] === '.') {
+              result.push([begin, {line: line, ch: character+1}, colors.end]);
+              ++character;
+              ++index;
+              break;
+            } else {
+              ++character;
+              ++index;
+            }
+          }
+        }
+
+        return result;
       });
   }
 
